a1158d75fe417b6bdeedb524742c0035c4afcecf
Ian Christian Myers <ian@iancmyers.com>
2010-08-01 19:32:07 -0700

    Added basic Unit class.

--- 10ktd.js
+++ 10ktd.js
@@ -19,7 +19,6 @@ Enemy.prototype = {
   hitFor : function (amount) {
     var self = this;
     self.hp = self.hp - amount;
-    
     if(self.hp < 1) self.die();
   },
   
@@ -39,5 +38,43 @@ Enemy.prototype = {
       self.div.remove();
       delete self;
     });
+  }
+}
+
+/*
+
+type = {
+  code : 1,
+  projectileSpeed: 800
+}
+
+*/
+
+function Unit (type, location) {
+  var self = this;
+  self.type = type;
+  self.location = location;
+  self.div = $('<div class="u' + type.code + '">');
+  
+  self.div.css({
+    top : location.y + 'px',
+    left : location.x + 'px'
+  });
+}
+
+Unit.prototype = {
+  fireAt : function (x, y) {
+    var self = this;
+    var projectile = $('<div class="w' + self.type.code + '">');
+    
+    projectile.css({
+      top : self.location.y + 'px',
+      left : self.location.x + 'px'
+    });
+    
+    projectile.animate({
+      top : y + 'px',
+      left : x + 'px'
+    }, self.type.projectileSpeed);
   }
 }
