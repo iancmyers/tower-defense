755bbea05333a7163bd1f626a23711597a246bcf
Ian Christian Myers <ian@iancmyers.com>
2010-08-18 22:48:08 -0700

    Added life logic. That's right, logic for LIFE.

--- 10ktd.js
+++ 10ktd.js
@@ -134,6 +134,11 @@
           $(unit).addClass('on');
         });
       },
+      
+      stop: function() {
+        alert('Game over, man. Game over.');
+        $('#b p, #b b, #b i').remove();
+      },
   
       click: function (event) {
         if (event.t=='slot') {
@@ -193,17 +198,6 @@
     }
   
   Game.start();
-
-  /*
-
-  type = {
-    code : 1,
-    speed : 500,
-    initialHP : 50
-  };
-
-  */
-  
   
   function Enemy(type, hpMultiplier, level) {
     var locationMark = 0,
@@ -227,6 +221,11 @@
           nextPoint: function () {
             var nextSlot = KEY_SLOTS[locationMark++], duration;
             if (!nextSlot) {
+              life--;
+              lifeEl.html(life);
+              if (life <= 0) {
+                Game.stop();
+              }
               el.remove();
               clearInterval(interval);
               return;
@@ -330,79 +329,4 @@
     slot.append(el);
     return self;
   }
-
-  /*
-  function Enemy (type, level) {
-    var self = this;
-    self.type = type;
-    self.hp = type.initialHP;
-    self.div = $('<div class="e' + type.code + ' l' + level + '">');
-  }
-
-  Enemy.prototype = {
-    hitFor : function (amount) {
-      var self = this;
-      self.hp = self.hp - amount;
-      if(self.hp < 1) self.die();
-    },
-  
-    moveTo : function (x, y) {
-      this.div.animate({
-        top : y + 'px',
-        left : x + 'px'
-      }, this.type.speed);
-    },
-  
-    die : function () {
-      var self = this;
-      self.div.animate({
-        width : '0px',
-        height : '0px'
-      }, 200, function() {
-        self.div.remove();
-        delete self;
-      });
-    }
-  }
-  */
-  
-  /*
-
-  type = {
-    code : 1,
-    projectileSpeed: 800
-  }
-
-  */
-/*
-  function Unit (type, location) {
-    var self = this;
-    self.type = type;
-    self.location = location;
-    self.div = $('<div class="u' + type.code + '">');
-  
-    self.div.css({
-      top : location.y + 'px',
-      left : location.x + 'px'
-    });
-  }
-
-  Unit.prototype = {
-    fireAt : function (x, y) {
-      var self = this;
-      var projectile = $('<div class="w' + self.type.code + '">');
-    
-      projectile.css({
-        top : self.location.y + 'px',
-        left : self.location.x + 'px'
-      });
-    
-      projectile.animate({
-        top : y + 'px',
-        left : x + 'px'
-      }, self.type.projectileSpeed);
-    }
-  }
-  */
-  
 }(this));
